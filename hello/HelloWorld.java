34444ab909ebed2d2ac8f0e8df58e5b15805231e
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-01-10 13:15:15 +0000

    adicionando a classe student no exemplo helloworld

--- hello/HelloWorld.java
+++ hello/HelloWorld.java
@@ -1,7 +1,11 @@
-package hello;
-
 public class HelloWorld {
     public static void main (String[] args) {
-        System.out.println("Hello World!");
+
+        Student student = new Student("Fabricio Santana", 42, "Computer Science");
+
+        System.out.printf("Hello, %s! You´re %s years old and are enroled in %s course.\n",
+                                        student.getName(),
+                                        student.getAge(),
+                                        student.getCourse());
     }
 }
