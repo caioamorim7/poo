e3706025d3b50f42884f90490654734da4d2685a
samuka7abr <samuelabrao2006@gmail.com>
2025-02-24 20:43:44 -0300

    fix: arrumando o nome da classe da tarefa 04-elephant

--- assignments/04-elephant/submissions/Samuel-Abrao/src/PassosElefante.java
+++ assignments/04-elephant/submissions/Samuel-Abrao/src/PassosElefante.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PassosElefante{
+public class ElefantePassos{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         
