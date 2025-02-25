5c9de046c2b323e08380684ebc476dd3d6829fdf
yurisantanaa <yuri.santana@discente.ufg.br>
2025-02-24 21:26:24 -0300

    corrige ex 02 poligono

--- assignments/02-area/submissions/yuri-santana/src/CalculadoraPoligono.java
+++ assignments/02-area/submissions/yuri-santana/src/CalculadoraPoligono.java
@@ -4,20 +4,20 @@ public class CalculadoraPoligono {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Digite o número de lados do polígono: ");
-    Double n = scanner.nextDouble();
+    int n = scanner.nextInt();
     System.out.println("Digite sua altura em metros: ");
     Double s = scanner.nextDouble();
     scanner.close();
     Double area = calcularArea(n, s);
-    String areaFormatada = formatarArea(area);
+    String areaFormatada = formatarSaida(area);
     System.out.println(areaFormatada);
   }
 
-  public static String formatarArea(Double area) {
-    return "A área do polígono é " + area + " metros quadrados";
+  public static String formatarSaida(Double area) {
+    return "A área do polígono é: " + area + " metros quadrados";
   }
 
-  public static Double calcularArea(Double n, Double s) {
+  public static Double calcularArea(int n, Double s) {
     return ((s*s)/4)*(n/(Math.tan(Math.PI/n)));
   }
 
