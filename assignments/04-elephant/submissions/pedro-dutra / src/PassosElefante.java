e0ce4145c0117e56c6db62701dd404c4e522ef6c
pedro_dutra <dutrapedro06@gmail.com>
2025-02-27 23:52:58 -0300

    adicionando as tarefas

---
+++ assignments/04-elephant/submissions/pedro-dutra / src/PassosElefante.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PassosElefante {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a posição da casa do amigo: ");
+        int x = scanner.nextInt();
+
+        if (x < 1 || x > 1000000) {
+            System.out.println("A posição deve estar entre 1 e 1.000.000.");
+            return;
+        }
+
+        int passos = calcularPassosMinimos(x);
+        String saida = formatarSaida(passos);
+
+        System.out.println(saida);
+    }
+
+    public static int calcularPassosMinimos(int x) {
+    
+        double resultado = (double) (x + 4) / 5;
+        return (int) Math.ceil(resultado);
+    }
+
+    public static String formatarSaida(int passos) {
+        return "O número mínimo de passos necessários é: " + passos;
+    }
+}
