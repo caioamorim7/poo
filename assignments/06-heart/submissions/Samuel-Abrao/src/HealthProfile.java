6945fff9d3306d470c07ee17e1f7638031b7a24a
samuka7abr <samuelabrao2006@gmail.com>
2025-03-05 13:59:54 -0300

    fix: TUDO DE ACORDO COM OS TESTES

--- assignments/06-heart/submissions/Samuel-Abrao/src/HealthProfile.java
+++ assignments/06-heart/submissions/Samuel-Abrao/src/HealthProfile.java
@@ -4,56 +4,54 @@ import java.time.Year;
 public class HealthProfile {
     private String firstName;
     private String lastName;
+    private char gender;
     private int dayOfBirth;
     private int monthOfBirth;
     private int yearOfBirth;
+    private int weight;
+    private int height;
 
-    public HealthProfile(String firstName, String lastName, int dayOfBirth, int monthOfBirth, int yearOfBirth) {
+    public HealthProfile(String firstName, String lastName, char gender, int dayOfBirth, int monthOfBirth, int yearOfBirth, int weight, int height) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.gender = gender;
         this.dayOfBirth = dayOfBirth;
         this.monthOfBirth = monthOfBirth;
         this.yearOfBirth = yearOfBirth;
+        this.weight = weight;
+        this.height = height;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public char getGender() {
+        return gender;
     }
 
     public int getDayOfBirth() {
         return dayOfBirth;
     }
 
-    public void setDayOfBirth(int dayOfBirth) {
-        this.dayOfBirth = dayOfBirth;
-    }
-
     public int getMonthOfBirth() {
         return monthOfBirth;
-    }
-
-    public void setMonthOfBirth(int monthOfBirth) {
-        this.monthOfBirth = monthOfBirth;
     }
 
     public int getYearOfBirth() {
         return yearOfBirth;
     }
 
-    public void setYearOfBirth(int yearOfBirth) {
-        this.yearOfBirth = yearOfBirth;
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getHeight() {
+        return height;
     }
 
     public int calculateAge() {
@@ -71,6 +69,10 @@ public class HealthProfile {
         return minTarget + " bpm - " + maxTarget + " bpm";
     }
 
+    public double calculateBMI() {
+        return weight / Math.pow(height / 100.0, 2);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -80,23 +82,28 @@ public class HealthProfile {
         System.out.print("Digite seu sobrenome: ");
         String lastName = scanner.nextLine();
 
+        System.out.print("Digite seu gênero (M/F): ");
+        char gender = scanner.next().charAt(0);
+
         System.out.print("Digite sua data de nascimento (dia mês ano separados por espaço): ");
         int day = scanner.nextInt();
         int month = scanner.nextInt();
         int year = scanner.nextInt();
 
+        System.out.print("Digite seu peso (kg): ");
+        int weight = scanner.nextInt();
+
+        System.out.print("Digite sua altura (cm): ");
+        int height = scanner.nextInt();
+
         scanner.close();
 
-        HealthProfile pessoa = new HealthProfile(firstName, lastName, day, month, year);
+        HealthProfile person = new HealthProfile(firstName, lastName, gender, day, month, year, weight, height);
 
-        int idade = pessoa.calculateAge();
-        int maxHeartRate = pessoa.calculateMaxHeartRate();
-        String targetHeartRate = pessoa.calculateTargetHeartRate();
-
-        System.out.println("\nNome: " + pessoa.getFirstName() + " " + pessoa.getLastName());
-        System.out.printf("Data de nascimento: %02d/%02d/%d%n", pessoa.getDayOfBirth(), pessoa.getMonthOfBirth(), pessoa.getYearOfBirth());
-        System.out.println("Idade: " + idade + " anos");
-        System.out.println("Frequência cardíaca máxima: " + maxHeartRate + " bpm");
-        System.out.println("Faixa de frequência cardíaca alvo: " + targetHeartRate);
+        System.out.println("\nNome: " + person.getFirstName() + " " + person.getLastName());
+        System.out.println("Idade: " + person.calculateAge() + " anos");
+        System.out.println("Frequência cardíaca máxima: " + person.calculateMaxHeartRate() + " bpm");
+        System.out.println("Faixa de frequência cardíaca alvo: " + person.calculateTargetHeartRate());
+        System.out.printf("IMC: %.2f%n", person.calculateBMI());
     }
 }
