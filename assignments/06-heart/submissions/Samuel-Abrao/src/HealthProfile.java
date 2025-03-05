32563931d45a42c89186a716ed8d3b3a9b31b6e2
samuka7abr <samuelabrao2006@gmail.com>
2025-03-05 14:18:18 -0300

    fix: refatoração da função de calcularBMI

--- assignments/06-heart/submissions/Samuel-Abrao/src/HealthProfile.java
+++ assignments/06-heart/submissions/Samuel-Abrao/src/HealthProfile.java
@@ -49,7 +49,8 @@ public class HealthProfile {
     }
 
     public double calculateBMI() {
-        return weight / Math.pow(height / 100.0, 2);
+        double heightInMeters = height / 100.0;
+        return weight / (heightInMeters * heightInMeters);
     }
 
     public static void main(String[] args) {
