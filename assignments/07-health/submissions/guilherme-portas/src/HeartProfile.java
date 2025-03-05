f6e6dc3699fcbe14aad1baf1a6bc977acde6ffe7
GoldenB00nie <91233541+GoldenB00nie@users.noreply.github.com>
2025-03-05 18:31:59 -0300

    Resolvido exercício 7

---
+++ assignments/07-health/submissions/guilherme-portas/src/HeartProfile.java
@@ -0,0 +1,121 @@
+import java.util.Scanner;
+
+public class HeartProfile {
+    private String firstName, lastName;
+    private char gender;
+    private int dayOfBirth, monthOfBirth, yearOfBirth;
+    private double heightInInches, weightInPounds;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite seu primeiro nome: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Digite seu sobrenome: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Digite seu Gênero (M/F): ");
+        char gender = scanner.next().charAt(0);
+        if(gender != 'm' && gender != 'M' && gender != 'f' && gender != 'F') {
+            scanner.close();
+            throw new java.lang.Error("Gênero inválido");
+        }
+
+        System.out.print("Digite sua data de nascimento (dia, mês e ano separados por espaço): ");
+        int dayOfBirth = scanner.nextInt(),
+            monthOfBirth = scanner.nextInt(),
+            yearOfBirth = scanner.nextInt();
+
+        System.out.print("Digite sua altura em polegadas: ");
+        double heightInInches = scanner.nextDouble();
+
+        System.out.print("Digite seu peso em libras: ");
+        double weightInPounds = scanner.nextDouble();
+
+        HeartProfile hr = new HeartProfile(firstName, lastName, gender, dayOfBirth, monthOfBirth, yearOfBirth, heightInInches, weightInPounds);
+
+        System.out.println("Nome: " + hr.getFirstName() + " " + hr.getLastName());
+        System.out.println("Gênero: " + hr.getGender());
+        System.out.println("Data de nascimento: " + hr.getDayOfBirth() + "/" + hr.getMonthOfBirth() + "/" + hr.getYearOfBirth());
+        System.out.println("Idade: " + hr.calculateAge(2025));
+        System.out.println("Altura: " + hr.getHeightInInches() + " Polegadas");
+        System.out.println("Peso: " + hr.getWeightInPounds() + " Libras");
+        System.out.printf("Índice de Massa Corporal (BMI): %.1f \n", hr.calculateBMI());
+        System.out.println("Frequência cardíaca máxima: " + hr.calculateMaxHeartRate());
+        System.out.println("Faixa de frequência cardíaca alvo: " + hr.calculateTargetHeartRate());
+        System.out.printf(  "%-13s Classificação \n" + 
+                            "%-13s Abaixo do peso \n" + 
+                            "%-13s Peso normal \n" +
+                            "%-13s Sobrepeso \n" +
+                            "%-13s Obesidade \n", 
+                            "BMI", "Menos de 18.5", "18.5 - 24.9", "25.0 - 29.9", "30.0 ou mais");
+
+        ////System.out.printf("%s, %s, %d, %d, %d", firstName, lastName, dayOfBirth, monthOfBirth, yearOfBirth);
+        
+        scanner.close();
+    }
+
+    public HeartProfile (String firstName, String lastName, char gender, int dayOfBirth, int monthOfBirth, int yearOfBirth, double heightInInches, double weightInPounds) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.dayOfBirth = dayOfBirth;
+        this.monthOfBirth = monthOfBirth;
+        this.yearOfBirth = yearOfBirth;
+        this.heightInInches = heightInInches;
+        this.weightInPounds = weightInPounds;
+    }
+
+    public String getFirstName () {
+        return this.firstName;
+    }
+
+    public String getLastName () {
+        return this.lastName;
+    }
+
+    public String getGender () {
+        if (this.gender == 'm' || this.gender == 'M') return "Masculino";
+        else return "Feminino";
+    }
+    
+    public int getDayOfBirth () {
+        return this.dayOfBirth;
+    }
+    
+    public int getMonthOfBirth () {
+        return this.monthOfBirth;
+    }
+
+    public int getYearOfBirth () {
+        return this.yearOfBirth;
+    }
+
+    public double getHeightInInches () {
+        return this.heightInInches;
+    }
+
+    public double getWeightInPounds () {
+        return this.weightInPounds;
+    }
+
+    public int calculateAge (int currentYear) {
+        if (this.yearOfBirth > currentYear) {
+            throw new java.lang.Error("Data de nascimento impossível ou ano atual inválido.");
+        }
+        else return currentYear - this.yearOfBirth;
+    }
+
+    public int calculateMaxHeartRate () {
+        return 220 - this.calculateAge(2025);
+    }
+
+    public String calculateTargetHeartRate () {
+        double max = (double) calculateMaxHeartRate();
+        return max * 0.5 + " bpm - " + max * 0.85 + " bpm";
+    }
+
+    public double calculateBMI() {
+        return this.weightInPounds * 703 / (this.heightInInches * this.heightInInches);
+    }
+}
