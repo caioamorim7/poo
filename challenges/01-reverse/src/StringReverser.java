1c7362ec0dd9dc11327af6f5907e968d92e297f0
pedro_dutra <dutrapedro06@gmail.com>
2025-03-07 09:41:58 -0300

    código arrumado

--- challenges/01-reverse/src/StringReverser.java
+++ challenges/01-reverse/src/StringReverser.java
@@ -8,20 +8,20 @@ public class StringReverser {
 
             System.out.print("Digite uma string: ");
             String input = scanner.nextLine();
-
             System.out.printf("String invertida: %s\n", reverse(input));
+
        }
     }
 
-    public String reverse(String inputString) {
+    public static String reverse(String inputString) {
 
-        if (inputString == "") {
+        if (inputString.equals("")) {
             throw new IllegalArgumentException("A string não pode ser nula.");
         }
 
         String reversedString = new String();
   
-        for (int i = inputString.length(); i > 0; i--) {
+        for (int i = inputString.length()-1; i >= 0; i--) {
             reversedString += inputString.charAt(i);
         }
         
