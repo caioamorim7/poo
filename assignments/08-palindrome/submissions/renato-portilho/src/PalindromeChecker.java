69045d338074b32e9e2a1d24f069163e4523baa0
Renato Portilho <renatoportilho79@gmail.com>
2025-03-07 08:24:50 -0300

    consertando palindromo

--- assignments/08-palindrome/submissions/renato-portilho/src/PalindromeChecker.java
+++ assignments/08-palindrome/submissions/renato-portilho/src/PalindromeChecker.java
@@ -3,41 +3,42 @@ import java.util.Scanner;
 public class PalindromeChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("Digite uma sequencia: ");
         String sequencia = scanner.next();
-        
+
         if (palindromo(sequencia)) {
             System.out.println("A sequência é um palíndromo!");
-        } else 
+        } else
             System.out.println("A sequência não é um palíndromo.");
-        
+
     }
 
-public static boolean palindromoValido(String input){
-    if (input == null || input.trim().isEmpty()) {
-        return false;
-    }else
-    
-    return true;
-}
+    public static boolean palindromoValido(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            return false;
+        } else
 
-public static boolean palindromo(String input){
-    if (!palindromoValido(input)) {
-        return false;
-    }else
-    String sanitizedInput = input.toLowerCase();
-    int length = sanitizedInput.length();
+            return true;
+    }
 
-    // Verifica se é um palíndromo usando `charAt` e `length`.
-    for (int i = 0; i < length / 2; i++) {
-        if (sanitizedInput.charAt(i) != sanitizedInput.charAt(length - 1 - i)) {
-            return false; // Se algum caractere não casar, não é um palíndromo.
-     
+    public static boolean palindromo(String input) {
+        String sanitizedInput = new String();
+        int length = 0;
+        if (!palindromoValido(input)) {
+            return false;
+        } else {
+            sanitizedInput = input.toLowerCase();
+            length = sanitizedInput.length();
         }
-return true;
+        // Verifica se é um palíndromo usando `charAt` e `length`.
+        for (int i = 0; i < length / 2; i++) {
+            if (sanitizedInput.charAt(i) != sanitizedInput.charAt(length - 1 - i)) {
+                return false; // Se algum caractere não casar, não é um palíndromo.
 
+            }
+
+        }
+        return true;
     }
 }
-
-
