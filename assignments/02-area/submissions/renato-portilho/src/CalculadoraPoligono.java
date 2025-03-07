55d11263d3864b3a55457e4b5f1fb8f7b154d638
Renato Portilho <renatoportilho79@gmail.com>
2025-03-07 08:08:45 -0300

    mudei o nome da classe que estava errado

--- assignments/02-area/submissions/renato-portilho/src/CalculadoraPoligono.java
+++ assignments/02-area/submissions/renato-portilho/src/CalculadoraPoligono.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculadoraPoligno {
+public class CalculadoraPoligono {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
