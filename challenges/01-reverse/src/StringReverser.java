cc4076be7fc8524424068a17c76ca812e2904819
ThiagoDellano <thiagodellano689@gmail.com>
2025-03-07 09:29:16 -0300

    Atividades/ Challenges Thiago D. e Gabriel D.

--- challenges/01-reverse/src/StringReverser.java
+++ challenges/01-reverse/src/StringReverser.java
@@ -1,35 +1,31 @@
 import java.util.Scanner;
 
-public class StringReverser {
+public class StringReverser1 {
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
+        
+        try(Scanner scanner = new Scanner(System.in)){
+
             System.out.print("Digite uma string: ");
             String input = scanner.nextLine();
 
-            // Verifica se a string é nula ou composta apenas por espaços
-            if (input == null || input.trim().isEmpty()) {
-                System.out.println("A string não pode ser vazia ou composta apenas por espaços.");
-            } else {
-                System.out.printf("String invertida: %s%n", reverseString(input));
-            }
-        }
+            System.out.printf("String invertida: %s\n", reverse(input));
+       }
     }
 
-    public static String reverseString(String inputString) {
-        if (inputString == null) {
+    public String reverse(String inputString) {
+
+        if (inputString == "") {
             throw new IllegalArgumentException("A string não pode ser nula.");
         }
 
-        int length = inputString.length();
-        char[] reversedArray = new char[length];
-
-        // Percorre a string de trás para frente e armazena no array
-        for (int i = 0; i < length; i++) {
-            reversedArray[i] = inputString.charAt(length - 1 - i);
+        String reversedString = new String();
+  
+        for (int i = inputString.length(); i > 0; i--) {
+            reversedString += inputString.charAt(i);
         }
-
-        // Retorna a string invertida
-        return new String(reversedArray);
+        
+        return reversedString;
+ 
     }
 }
