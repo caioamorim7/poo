fd8fcca936d57ea0b3237cd106269928128acf54
Renato Portilho <renatoportilho79@gmail.com>
2025-03-10 09:47:56 -0300

    conserto exercicio 6

--- assignments/06-heart/submissions/renato-portilho/src/HeartRates.java
+++ assignments/06-heart/submissions/renato-portilho/src/HeartRates.java
@@ -1,39 +1,24 @@
-import java.util.Scanner;
 
 public class HeartRates {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-         System.out.println("Digite seu Primeiro nome: ");
-         String primeiroNome = scanner.nextLine();
-
-         System.out.println("Digite seu sobrenome: ");
-         String sobrenome = scanner.nextLine();
-
-         
-         System.out.println("Digite a data do seu nascimento: ");
-         int dia = scanner.nextInt(), mes = scanner.nextInt(), ano = scanner.nextInt();
-         
-         System.out.println("Nome: " +primeiroNome+ " " + sobrenome);
-         System.out.println("Data de nascimento: " +dia+ "/" +mes+ "/" +ano);
-
-         int idade = calcularIdade(dia, mes, ano);
-         System.out.println("Idade: " +idade);
-
-         int frequenciaMaxima = calcularfrequenciaMaxima(idade);
-         System.out.println("Frequência cardíaca máxima: "+frequenciaMaxima+ "bpm");
-
-         String frequenciaIdeal = frequenciaCardiacaIdeal(frequenciaMaxima);
-         System.out.println(frequenciaIdeal);
-
-
-        
-
-         
-
+    private String firstName;
+    private String lastName;
+    private int dayOfBirth;
+    private int monthOfBirth;
+    private int yearOfBirth;
+    
+    public HeartRates(String firstName, String lastName, int dayOfBirth, int monthOfBirth, int yearOfBirth){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dayOfBirth = dayOfBirth;
+        this.yearOfBirth = yearOfBirth;
+        this.monthOfBirth = monthOfBirth;
 
 
     }
-    public static int  calcularIdade(int dia, int mes, int ano){
+
+
+ 
+    public int calcularIdade(int dia, int mes, int ano){
                
         int idade;
                 if(dia < 0 || dia > 31 || mes > 12 || mes < 0 || ano > 2025 || ano < 1900){
@@ -58,18 +43,78 @@ public class HeartRates {
         return idade;
     }
 
-    public static int calcularfrequenciaMaxima(int idade){
+    public int calcularfrequenciaMaxima(int idade){
          
         return 220 - idade;
 
     }
 
-    public static String frequenciaCardiacaIdeal(int frequenciaMaxima){
+    public String frequenciaCardiacaIdeal(int frequenciaMaxima){
         int frequenciaMaior = (int) (0.8 * frequenciaMaxima);
         int frequenciaMenor = (int) (0.5 * frequenciaMaxima);
 
-        return "Frequência cardiaca ideal: "+frequenciaMenor+ "bpm - " +frequenciaMaior+ "bpm";
+        return "Frequência cardiaca ideal: "+frequenciaMenor+ " bpm - " +frequenciaMaior+ " bpm";
 
+    }
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+
+    public int getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+
+
+    public void setDayOfBirth(int dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+
+
+
+    public int getMonthOfBirth() {
+        return monthOfBirth;
+    }
+
+
+
+    public void setMonthOfBirth(int monthOfBirth) {
+        this.monthOfBirth = monthOfBirth;
+    }
+
+
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
     }
 }
 
