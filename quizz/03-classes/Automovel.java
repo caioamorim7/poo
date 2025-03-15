9495ba3a88bc1c3d61b651bc80e3d6d98391e79e
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-03-15 21:43:27 +0000

    códigos para quizz

---
+++ quizz/03-classes/Automovel.java
@@ -0,0 +1,16 @@
+public class Automovel {
+    Motor motor;
+    Roda rodas;
+}
+
+class Motor {
+    int potencia;
+    int cilindrada;
+    String tipo;
+}
+
+class Roda {
+    int aro;
+    int largura;
+    String marca;
+}
---
+++ quizz/03-classes/Procurador.java
@@ -0,0 +1,13 @@
+public class Procurador {
+    private String numOAB;
+    private String nome;
+    private String cpf;
+        
+    public Procurador (){ }
+
+    public Procurador (String numOAB, String nome, String cpf){
+        this.numOAB = numOAB;
+        this.nome = nome;
+        this.cpf = cpf;
+     }
+} 
