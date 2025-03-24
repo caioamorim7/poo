9902834cdaa145262f78cccf62fc2c7e7fa13577
yurisantanaa <yuri.santana@discente.ufg.br>
2025-03-24 10:04:25 -0300

    ipd bank pt 2

--- challenges/02-bank/submissions/yuri-santana/src/IDPBank.java
+++ challenges/02-bank/submissions/yuri-santana/src/IDPBank.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
+import models.Account;
+import models.CheckingAccount;
+import models.Customer;
+import models.SalaryAccount;
+import models.SavingsAccount;
 
 public class IDPBank {
   public static void main(String[] args) {
     int n = 0;
-    while(n != 5) {
+    while (n != 5) {
       System.out.println(""
-      + "Seja bem-vindo ao IDP Bank! Escolha uma das opções abaixo:\n"
-      + "1. Abrir conta\n"
-      + "2. Consultar cliente\n"
-      + "3. Depositar\n"
-      + "4. Sacar\n"
-      + "5. Sair\n"
-      + "Digite o número correspondente à opção desejada: ");
+          + "Seja bem-vindo ao IDP Bank! Escolha uma das opções abaixo:\n"
+          + "1. Abrir conta\n"
+          + "2. Consultar cliente\n"
+          + "3. Depositar\n"
+          + "4. Sacar\n"
+          + "5. Tranferir"
+          + "6. Sair\n"
+          + "Digite o número correspondente à opção desejada: ");
 
       @SuppressWarnings("resource")
       Scanner scanner = new Scanner(System.in);
@@ -27,7 +33,7 @@ public class IDPBank {
           System.out.println("CPF: ");
           String cpf = scanner.next();
           System.out.println();
-          Custumer custumer = createCustumer(firstName, lastName, cpf);
+          Customer custumer = createCustomer(firstName, lastName, cpf);
           openAccount(custumer);
           System.out.println("Conta criada com sucesso!\n");
           System.out.println(custumer.displayInformation());
@@ -44,24 +50,42 @@ public class IDPBank {
       }
     }
   }
-  
-  public static Custumer createCustumer(String firstName, String lastName, String cpf){
-    Custumer custumer = new Custumer(firstName, lastName, cpf);
+
+  public static Customer createCustomer(String firstName, String lastName, String cpf) {
+    Customer custumer = new Customer(firstName, lastName, cpf);
     return custumer;
   }
-  
-  public static Account openAccount(Custumer custumer){
-    Account account = new Account();
-    custumer.addAccount(account);
-    return account;
+
+  public static Account openAccount(Customer customer) {
+    System.out.println("""
+        Qual tipo de conta deseja abrir?
+        1. Conta Corrente
+        2. Conta Poupança
+        3. Conta Salário
+
+        Digite o número da opção desejada:
+        """);
+    Scanner scanner = new Scanner(System.in);
+    int tipo = scanner.nextInt();
+    switch (tipo) {
+      case 1 -> {
+        CheckingAccount checkingAccount = new CheckingAccount();
+      }
+      case 2 -> {
+        SavingsAccount savingsAccount = new SavingsAccount();
+      }
+      case 3 -> {
+        SalaryAccount salaryAccount = new SalaryAccount();
+      }
+    }
+    return null;
   }
-  
-  public boolean deposit(double amount){
+
+  public boolean deposit(double amount) {
     return false;
   }
-  
-  public boolean withdraw(double amount){
+
+  public boolean withdraw(double amount) {
     return false;
   }
 }
-  
