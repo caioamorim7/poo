8255925089f276293b40236977b2c9bcb5e6e62b
ArturMSS <arturmoraesilva@gmail.com>
2025-04-25 09:06:03 -0300

    correção Motorcycle

--- challenges/03-parking/submissions/artur-moraes/src/Motorcycle.java
+++ challenges/03-parking/submissions/artur-moraes/src/Motorcycle.java
@@ -11,7 +11,7 @@ public class Motorcycle extends Vehicle {
 
     @Override //sobrescreve o metodo toString
     public String toString(){
-        return "Veículo: carro - " + super.toString(); // pega de vehicle
+        return "Veículo: moto - " + super.toString(); // pega de vehicle
     }
     
 }
