d2c1aa98713acaeccdec464f858f9cb95abe3e89
Kayke-Queiroz <kaykekato@gmail.com>
2025-05-08 20:12:20 -0300

    Update ValidationException.java

--- assignments/14-exception/submissions/kayke-queiroz/src/ValidationException.java
+++ assignments/14-exception/submissions/kayke-queiroz/src/ValidationException.java
@@ -1,11 +1,9 @@
 public class ValidationException extends Exception {
 
-    // Construtor que recebe apenas a mensagem de erro
     public ValidationException(String message) {
         super(message);
     }
 
-    // Construtor que recebe a mensagem e a causa da exceção
     public ValidationException(String message, Throwable cause) {
         super(message, cause);
     }
