231076125a85af8be1000337a15ac02fe2ffab55
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-06-02 03:39:45 +0000

    Exemplo CRUD

---
+++ lectures/examples/14-db/src/DeleteCategory.java
@@ -0,0 +1,24 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Properties;
+
+public class DeleteCategory {
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.load(Files.newInputStream(Paths.get("db.properties")));
+
+        try (Connection conn = DriverManager.getConnection(
+                props.getProperty("url"), props.getProperty("user"), props.getProperty("password"))) {
+
+            String sql = "DELETE FROM category WHERE name = ?";
+            try (PreparedStatement ps = conn.prepareStatement(sql)) {
+                ps.setString(1, "EXEMPLO_CATEGORIA");
+                int rows = ps.executeUpdate();
+                System.out.println("Linhas removidas: " + rows);
+            }
+        }
+    }
+}
