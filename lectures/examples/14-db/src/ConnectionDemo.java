379fa01facd83ecc39d14a9975e335b458c8c87d
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-06-06 02:07:25 +0000

    refactoring

--- lectures/examples/14-db/src/ConnectionDemo.java
+++ lectures/examples/14-db/src/ConnectionDemo.java
@@ -11,22 +11,28 @@ public class ConnectionDemo {
 
         try{
             Properties props = new Properties();
-            props.load(Files.newInputStream(Paths.get("db.properties")));
+            
+            try (var inputStream = Files.newInputStream(Paths.get("db.properties"))) {
+                props.load(inputStream);
+            }
 
             String url = props.getProperty("url"); //"jdbc:postgresql://localhost:5432/postgres";
             String usuario = props.getProperty("user"); // "postgres";
             String senha = props.getProperty("password"); // *****;
 
-                try (Connection conexao = DriverManager.getConnection(url, usuario, senha)) {
-                    System.out.println("Conexão estabelecida com sucesso!");
-                    System.out.println(conexao.getClientInfo());
-                    System.out.println(conexao.getSchema());
-                } catch (SQLException e) {
-                    System.err.println("Erro na conexão: " + e.getMessage());
-                }
+            connectToDatabase(url, usuario, senha);
 
         } catch(IOException ioex){
             System.err.println("Erro ao ler arquivo de configuração: " + ioex.getMessage());
+        }
+    }
+    private static void connectToDatabase(String url, String usuario, String senha) {
+        try (Connection conexao = DriverManager.getConnection(url, usuario, senha)) {
+            System.out.println("Conexão estabelecida com sucesso!");
+            System.out.println(conexao.getClientInfo());
+            System.out.println(conexao.getSchema());
+        } catch (SQLException e) {
+            System.err.println("Erro na conexão: " + e.getMessage());
         }
     }
 }
