a06bbb95371c60c445db64a8a051a84448f397a4
ffsant <ffsant@senado.leg.br>
2025-06-28 14:58:55 +0000

    ajustando o servidor e retirando a senha

--- challenges/07-flix/submissions/renato-portilho/src/ConexaoSupabase.java
+++ challenges/07-flix/submissions/renato-portilho/src/ConexaoSupabase.java
@@ -4,9 +4,9 @@ import java.sql.SQLException;
 
 public class ConexaoSupabase {
 
-    private static final String URL = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:6543/postgres";
-    private static final String USER = "postgres.uqwaibkphzdntrxwzvbe";
-    private static final String PASSWORD = "050914Renato"; 
+    private static final String URL = "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:5432/postgres";
+    private static final String USER = "postgres.snghnxpdoffuasscqmms";
+    private static final String PASSWORD = ""; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
