55972822a166634b9bc6adc06d9d829d0a1a2e8f
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-07-07 13:57:13 +0000

    limpando repositório para o próximo semestre

--- assignments/14-exception/submissions/guilherme-portas/src/ValidationException.java
+++
@@ -1,9 +0,0 @@
-public class ValidationException extends Exception{
-    public ValidationException(String msgErro) {
-        super(msgErro);
-    }
-
-    public ValidationException(Throwable causa) {
-
-    }
-}
--- assignments/15-duplicate/submissions/kayke-queiroz/src/Util.java
+++
@@ -1,6 +0,0 @@
-public class Util {
-    public static String capitalize(String nome) {
-        if (nome.isEmpty()) return nome;
-        return nome.substring(0, 1).toUpperCase() + nome.substring(1).toLowerCase();
-    }
-}
