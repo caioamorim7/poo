df5d304a7cd703f2be977b2aed02d2bcde5f4b85
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-07-07 21:17:08 +0000

    removendo código dos desafios

--- challenges/02-bank/submissions/artur-moraes/src/SavingsAccount.java
+++
@@ -1,8 +0,0 @@
-public class SavingsAccount extends Account {
-    private final double interestRate = 0.005;
-
-    public void applyInterest() {
-        double interest = getBalance() * interestRate;
-        deposit(interest);
-    }
-}
