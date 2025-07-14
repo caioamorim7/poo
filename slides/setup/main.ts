2e86b93bd4127df22d0f66aa3d5a15bc7836b90f
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-07-14 19:31:39 +0000

    refatorando

--- slides/setup/main.ts
+++ slides/setup/main.ts
@@ -1,7 +1,7 @@
 // setup/main.ts
 import { defineAppSetup } from '@slidev/types'
-import FigureWithCaption from '../pages/components/FigureWithCaption.vue'
-import VideoWithQuote from '../pages/components/VideoWithQuote.vue'
+import FigureWithCaption from '../components/FigureWithCaption.vue'
+import VideoWithQuote from '../components/VideoWithQuote.vue'
 
 
 export default defineAppSetup(({ app }) => {
