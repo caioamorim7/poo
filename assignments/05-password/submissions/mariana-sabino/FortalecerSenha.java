c113ef51c1d60d9f10a6881210bcbba23b811f17
marianassabino <marisabmor@gmail.com>
2025-08-21 23:28:26 -0300

    .

--- assignments/05-password/submissions/mariana-sabino/FortalecerSenha.java
+++ assignments/05-password/submissions/mariana-sabino/FortalecerSenha.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class FortalecerSenha {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner (System.in);
+        Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Digite a senha: ");
+        System.out.print("Digite a senha: ");
         String senha = leitor.nextLine();
 
         if (senha.length() < 1 || senha.length() > 10) {
@@ -13,18 +13,17 @@ public class FortalecerSenha {
             return;
         }
 
-        String senhaFortalecida = fortalecerSenha(senha);
+        String senhaFortalecida = fortalecerSenha(senha); // CORREÇÃO AQUI
         System.out.println("Senha fortalecida: " + senhaFortalecida);
 
         leitor.close();
-
     }
 
-    public static String fortalecerSenha(String s){
+    public static String fortalecerSenha(String s) {
         String melhorSenha = "";
-        int maiorTempo = -1; 
+        int maiorTempo = -1;
+
         for (char letra = 'a'; letra <= 'z'; letra++) {
-            // Testa todas as posições possíveis
             for (int i = 0; i <= s.length(); i++) {
                 String novaSenha = s.substring(0, i) + letra + s.substring(i);
                 int tempo = calcularTempoDigitacao(novaSenha);
