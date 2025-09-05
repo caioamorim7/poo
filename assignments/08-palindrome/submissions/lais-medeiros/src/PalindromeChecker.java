0893f0a288e37d9fbd5ab0cb84517c193b92b6d8
lais <lvitalmedeiros@gmail.com>
2025-09-04 21:10:27 -0300

    Atividades #2

---
+++ assignments/08-palindrome/submissions/lais-medeiros/src/PalindromeChecker.java
@@ -0,0 +1,24 @@
+
+class PalindromeChecker {
+
+    public static boolean isValidInput(String input) {
+        if (input == null) return false;
+        input = input.trim();
+        return !input.isEmpty();
+    }
+
+    public static boolean isPalindrome(String input) {
+        input = input.toLowerCase(); 
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
