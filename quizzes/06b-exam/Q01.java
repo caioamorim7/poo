b5cd2c4a270be7d489750fb5018073f3e6b74b48
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-09-30 20:50:45 +0000

    removendo

--- quizzes/06b-exam/Q01.java
+++
@@ -1,9 +0,0 @@
-public class Q01 {
-    public static void main(String[] args) {
-        var version = Runtime.version();
-        var vmName = System.getProperty("java.vm.name");
-        int space = vmName.indexOf(' ');
-        String tag = space > 0 ? vmName.substring(0, space) : vmName;
-        System.out.println(tag + "-" + version.feature());
-    }
-}
