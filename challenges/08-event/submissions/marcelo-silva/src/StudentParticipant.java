a434498dda5bb3ebda216f53476f11493d62362c
marcelo <marcelo_sn3@hotmail.com>
2025-10-03 10:00:52 -0300

    prova POO

---
+++ challenges/08-event/submissions/marcelo-silva/src/StudentParticipant.java
@@ -0,0 +1,20 @@
+public class StudentParticipant extends Participant {
+    private String course;
+    private int currentSemester;
+
+    public StudentParticipant(String id, String fullName, String email, String course, int currentSemester) {
+        super(id, fullName, email);
+        this.course = course;
+        this.currentSemester = currentSemester;
+    }
+
+    @Override
+    public String describeRole() {
+        return "Estudante do curso de " + this.course + ", cursando o " + this.currentSemester + "º semestre.";
+    }
+
+    @Override
+    public String toString() {
+        return "StudentParticipant[id=" + getId() + ", fullName=" + getFullName() + ", email=" + getEmail() + ", course=" + course + ", currentSemester=" + currentSemester + "]";
+    }
+}
