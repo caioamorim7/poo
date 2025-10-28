6609b86cb7f678f756ee1f4facbedfc13846baa1
OrnelasBallarin <ornelas.ballarin@gmail.com>
2025-10-28 20:16:58 -0300

    TreeSet

--- classroom/07-collections/submissions/felipe-ballarin/src/TreeSetApp.java
+++ classroom/07-collections/submissions/felipe-ballarin/src/TreeSetApp.java
@@ -1,74 +1,145 @@
 import java.util.ArrayList;
+import java.util.TreeSet;
+import static java.lang.System.out;
+
 
 public class TreeSetApp {
     public static void main(String[] args) {
-        ArrayList<String> cores = new ArrayList<>();
 
         //1. ___________________________________
-        System.out.println("\n1.");
+        out.println("\n1.");
+
+        TreeSet<String> cores = new TreeSet<>();
+        cores.add("Vermelho");
+        cores.add("Azul");
+        cores.add("Verde");
+        cores.add("Amarelo");
+        cores.add("Roxo");
+
+        out.println("TreeSet em ordem alfabética: " + cores);
+
 
         //2. ___________________________________
-        System.out.println("\n2. ");
+        out.println("\n2. ");
+
+        out.println("Iterarando os Elementos: ");
+        for (String cor : cores) {
+            out.println(cor);
+        }
+
 
         //3. ___________________________________
-        System.out.println("\n3. ");
+        out.println("\n3. ");
+
+        TreeSet<String> cores2 = new TreeSet<>();
+        TreeSet<String> maisCores = new TreeSet<>();
+
+        cores2.add("Preto");
+        cores2.add("Branco");
+
+        out.println("Novo TreeSet cores2: " + cores2);
+
+        maisCores.addAll(cores);
+
+        out.println("Novo conjunto 'maisCores'com tudo: " + maisCores);
+
 
         //4. ___________________________________
-        System.out.println("\n4.");
+        out.println("\n4.");
+
+        out.println("maisCores em ordem reversa: " + maisCores.descendingSet());
+
 
         //5. ___________________________________
-        System.out.println("\n5.");
+        out.println("\n5.");
+
+        out.println("Primeiro elemento: " + maisCores.first());
+        out.println("Último elemento: " + maisCores.last()); 
+
 
         //6. ___________________________________
-        System.out.println("\n6.");
+        out.println("\n6.");
+
+        TreeSet<String> cloneCores = (TreeSet<String>) cores.clone();
+        out.println("Clone: " + cloneCores);
+
 
         //7. ___________________________________
-        System.out.println("\n7.");
+        out.println("\n7.");
+
+        out.println("Quantidade de cores: " + cores.size()); 
+
 
         //8. ___________________________________
-        System.out.println("\n8.");
+        out.println("\n8.");
+
+        out.println("cores e cores2 são iguais? " + cores.equals(cores)); 
+
 
         //9. ___________________________________
-        System.out.println("\n9.");
+        out.println("\n9.");
+
+        TreeSet<Integer> numeros = new TreeSet<>();
+        numeros.add(1);
+        numeros.add(12);
+        numeros.add(4);
+        numeros.add(5);
+        numeros.add(46);
+
+        out.println("Set original: " + numeros);
+        out.println("numeros < 7: " + numeros.headSet(7));
+
 
         //10. ___________________________________
-        System.out.println("\n10.");
+        out.println("\n10.");
+
+        out.println("Elemento 'ceiling' maior ou igual a 10: " + numeros.ceiling(10));
+
 
         //11. ___________________________________
-        System.out.println("\n11.");
+        out.println("\n11.");
+
+        out.println("Elemento 'floor' menor ou igual a 10: " + numeros.floor(10));
+
 
         //12. ___________________________________
-        System.out.println("\n12.");
+        out.println("\n12.");
+
+        out.println("Elemento 'higher' estritamente maior que 5: " + numeros.higher(5));
+
 
         //13. ___________________________________
-        System.out.println("\n13.");
+        out.println("\n13.");
+
+        out.println("Elemento 'lower' estritamente menor que 5: " + numeros.lower(5));
+
 
         //14. ___________________________________
-        System.out.println("\n14.");
+        out.println("\n14.");
+
+        Integer primeiro = numeros.pollFirst();
+
+        out.println("Elemento removido (primeiro): " + primeiro);
+        out.println("treeSet numeros apos pollFirst: " + numeros);
+
 
         //15. ___________________________________
-        System.out.println("\n15.");
+        out.println("\n15.");
+
+        Integer ultimo = numeros.pollLast();
+
+        out.println("Elemento removido (ultimo): " + ultimo);
+        out.println("treeSet apos pollLast: " + numeros);
+
 
         //16. ___________________________________
-        System.out.println("\n16.");
+        out.println("\n16.");
 
-        //17. ___________________________________
-        System.out.println("\n17.");
+        boolean removeu = numeros.remove(12);
 
-        //18. ___________________________________
-        System.out.println("\n18.");
+        out.println("Removeu o 12? " + removeu);
+        out.println("Set numeros depois de remover: " + numeros);
 
-        //19. ___________________________________
-        System.out.println("\n19.");
-
-        //20. ___________________________________
-        System.out.println("\n20.");
-
-        //21. ___________________________________
-        System.out.println("\n21.");
-
-        //22. ___________________________________
-        System.out.println("\n22.");
 
     }
 }
