97e881175db4dcc114ee061b3102067448a7353a
FABRICIO FERNANDES SANTANA <fabricio.santana@gmail.com>
2025-11-11 18:56:18 +0000

    mais casos de testees

--- codeforces/set-matrix-zeroes/test/SetMatrixZeroesTest.java
+++ codeforces/set-matrix-zeroes/test/SetMatrixZeroesTest.java
@@ -79,6 +79,101 @@ public class SetMatrixZeroesTest {
         assertMatrixEquals(expected, matrix);
     }
 
+    @Test
+    public void setMatrixZeroes_sampleCase_oneZeroInMiddle() {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 0, 9}
+        };
+
+        int[][] expected = {
+                {1, 0, 3},
+                {4, 0, 6},
+                {0, 0, 0}
+        };
+
+        SetMatrixZeroes.setMatrixZeroes(matrix);
+
+        assertMatrixEquals(expected, matrix);
+    }
+
+    @Test
+    public void setMatrixZeroes_sampleCase_noZeros() {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {4, 5, 6, 7},
+                {8, 9, 4, 6}
+        };
+
+        int[][] expected = copyMatrix(matrix);
+
+        SetMatrixZeroes.setMatrixZeroes(matrix);
+
+        assertMatrixEquals(expected, matrix);
+    }
+
+    @Test
+    public void setMatrixZeroes_sampleCase_multipleZerosLargeMatrix() {
+        int[][] matrix = {
+                {1, 1, 0, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 0, 1, 1, 1},
+                {1, 1, 1, 1, 1}
+        };
+
+        int[][] expected = {
+                {0, 0, 0, 0, 0},
+                {1, 0, 0, 1, 1},
+                {1, 0, 0, 1, 1},
+                {0, 0, 0, 0, 0},
+                {1, 0, 0, 1, 1}
+        };
+
+        SetMatrixZeroes.setMatrixZeroes(matrix);
+
+        assertMatrixEquals(expected, matrix);
+    }
+
+    @Test
+    public void setMatrixZeroes_sampleCase_centerZeroInSquare() {
+        int[][] matrix = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+
+        int[][] expected = {
+                {1, 0, 1},
+                {0, 0, 0},
+                {1, 0, 1}
+        };
+
+        SetMatrixZeroes.setMatrixZeroes(matrix);
+
+        assertMatrixEquals(expected, matrix);
+    }
+
+    @Test
+    public void setMatrixZeroes_sampleCase_zeroInLastColumnAndMiddle() {
+        int[][] matrix = {
+                {3, 5, 2, 0},
+                {1, 0, 4, 6},
+                {7, 3, 2, 4}
+        };
+
+        int[][] expected = {
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {7, 0, 2, 0}
+        };
+
+        SetMatrixZeroes.setMatrixZeroes(matrix);
+
+        assertMatrixEquals(expected, matrix);
+    }
+
     private static void assertMatrixEquals(int[][] expected, int[][] actual) {
         for (int row = 0; row < expected.length; row++) {
             assertArrayEquals(expected[row], actual[row], "Row " + row + " mismatch");
